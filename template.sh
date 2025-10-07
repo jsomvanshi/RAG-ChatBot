e0213d6f1d08894621bffda38cf2b293028c4f1e
jsomvanshi <jayeshsomvanshi77@gmail.com>
2025-10-07 16:19:45 +0200

    folder structure added

---
+++ template.sh
@@ -0,0 +1,16 @@
+#creating mkdir
+mkdir -p src
+mkdir -p resource
+
+#creating file
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+touch README.md
+
+echo "Successfully created"
