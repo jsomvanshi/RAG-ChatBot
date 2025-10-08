6cf6bfdca6bff3a863899083644f16f531d3f179
jsomvanshi <jayeshsomvanshi77@gmail.com>
2025-10-08 17:34:23 +0200

    requirements added

---
+++ README.md
@@ -0,0 +1,8 @@
+create a virtual environment 
+conda create -m medibot python=3.10 -y
+ activate env
+ conda activate medibot
+
+
+install req
+
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+      name ="medical_chatbot",
+      version="0.1.0",
+      author="Jayesh Somvanshi",
+      author_email= "jayeshsomvanshi77@gmail.com",
+      packages=find_packages(),
+      install_requires=[]
+)
